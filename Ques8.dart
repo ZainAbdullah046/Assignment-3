3f5449ad936ebc94079086087a9bb7688002d9c3
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-02-08 23:39:55 +0500

    cube of the number

---
+++ Ques8.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number = 5;
+  int result = 1;
+  for (int i = 1; i <= number; i++) {
+    result = i * i * i;
+    print("Number is : $i and cube of the $i is :$result");
+  }
+}
