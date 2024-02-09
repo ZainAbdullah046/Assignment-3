dfcc2a56558bc8688803df287afa415e4bc50f18
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-02-09 23:37:25 +0500

    pyarmid of the increasing digits

---
+++ Ques13.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+
+void main() {
+  int count = 1;
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 5 - i; j++) {
+      stdout.write(" ");
+    }
+    for (int z = 1; z < i; z++) {
+      stdout.write(" $count");
+
+      count++;
+    }
+    stdout.writeln();
+  }
+}
