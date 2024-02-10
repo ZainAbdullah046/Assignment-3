012d2e9a81781a6f5e8f636ebce57209831f582f
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-02-10 23:20:54 +0500

    login  check with loop

--- Ques15.dart
+++ Ques15.dart
@@ -1,17 +1,16 @@
 import "dart:io";
 
 void main() {
-  stdout.write("Enter the email :");
-  String email = stdin.readLineSync()!;
-  stdout.write("Enter the password :");
-  String password = stdin.readLineSync()!;
-  if (email == "zain@" && password == "zain123") {
-    print("login sucessfull");
-  } else if (email == "zain@" && password != "zain123") {
-    print("login unsucessfull because of password");
-  } else if (email != "zain@" && password == "zain123") {
-    print("login unsucessfull because of email");
-  } else {
-    print("login unsucessfull because of both email and password");
+  while (true) {
+    stdout.write("Enter the email :");
+    String email = stdin.readLineSync()!;
+    stdout.write("Enter the password :");
+    String password = stdin.readLineSync()!;
+    if (email == "zain@" && password == "zain123") {
+      print("login sucessfull");
+      break;
+    } else {
+      print("login unsucessfull try again !");
+    }
   }
 }
